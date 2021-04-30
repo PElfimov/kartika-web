9aa5dd0200b7e78bfd4ff008d89dde68eb89348e
Pavel Elfimov <pelfimov@yandex.ru>
2021-04-30 11:26:04 +0700

    start refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import styles from "./App.module.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className={styles.wrapper}>
+
+            <aside className={styles.header}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam fuga, ratione ipsum nostrum neque
+                placeat voluptatem totam eaque cupiditate consectetur.
+            </aside>
+            <main className={styles.main}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias veniam, vel ipsam est consectetur
+                inventore. Dolorem aut minima excepturi beatae!
+            </main>
+        </div>
+    )
 }
 
-export default App;
+export default App
+
