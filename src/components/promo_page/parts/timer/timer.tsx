a16064e3dac924dec88c85a7110c7baf8ee59a6d
Pavel Elfimov <pelfimov@yandex.ru>
2021-05-31 15:01:44 +0700

    timer is work

--- src/components/promo_page/parts/timer/timer.tsx
+++ src/components/promo_page/parts/timer/timer.tsx
@@ -1,46 +1,56 @@
 import {Component} from "react"
+import Countdown from "react-countdown"
 import styles from "./timer.module.css"
 
 interface Props {}
+
 export class Timer extends Component<Props, {}> {
   render() {
-    return (
-      <div className={styles.root}>
-        <div className={styles.photo1}></div>
-        <div className={styles.photo2}></div>
-        <div className={styles.body}>
-          <div className={styles.title}>До конца акции осталось:</div>
-          <div className={styles.clock}>
-            <div className={`${styles.clockNumber} ${styles.clockDay}`}>
-              <p className={styles.lineUp}>
-                <span id="days">0</span>
-              </p>
-              <p className={styles.lineDown}>дней</p>
-            </div>
-            <div className={styles.clockColon}>:</div>
-            <div className={styles.clockNumber}>
-              <p className={styles.lineUp}>
-                <span id="hours">3</span>
-              </p>
-              <p className={styles.lineDown}>часов</p>
-            </div>
-            <div className={styles.clockColon}>:</div>
-            <div className={styles.clockNumber}>
-              <p className={styles.lineUp}>
-                <span id="minutes">2</span>
-              </p>
-              <p className={styles.lineDown}>минут</p>
-            </div>
-            <div className={styles.clockColon}>:</div>
-            <div className={styles.clockNumber}>
-              <p className={styles.lineUp}>
-                <span id="seconds">35</span>
-              </p>
-              <p className={styles.lineDown}>секунд</p>
-            </div>
+    return <Countdown date={Date.now() + 200000000} renderer={HTML} />
+  }
+}
+
+// type HTMLProps = {
+//   days: number; hours: number; minutes: number; seconds: number
+// };
+
+function HTML({days, hours, minutes, seconds}: {days: number; hours: number; minutes: number; seconds: number}) {
+  return (
+    <div className={styles.root}>
+      <div className={styles.photo1}></div>
+      <div className={styles.photo2}></div>
+      <div className={styles.body}>
+        <div className={styles.title}>До конца акции осталось:</div>
+        <div className={styles.clock}>
+          <div className={`${styles.clockNumber} ${styles.clockDay}`}>
+            <p className={styles.lineUp}>
+              <span>{days}</span>
+            </p>
+            <p className={styles.lineDown}>дней</p>
+          </div>
+          <div className={styles.clockColon}>:</div>
+          <div className={styles.clockNumber}>
+            <p className={styles.lineUp}>
+              <span>{hours}</span>
+            </p>
+            <p className={styles.lineDown}>часов</p>
+          </div>
+          <div className={styles.clockColon}>:</div>
+          <div className={styles.clockNumber}>
+            <p className={styles.lineUp}>
+              <span>{minutes}</span>
+            </p>
+            <p className={styles.lineDown}>минут</p>
+          </div>
+          <div className={styles.clockColon}>:</div>
+          <div className={styles.clockNumber}>
+            <p className={styles.lineUp}>
+              <span>{seconds}</span>
+            </p>
+            <p className={styles.lineDown}>секунд</p>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
