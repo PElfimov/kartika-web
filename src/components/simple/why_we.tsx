aba571f1a9a8c39a8ebb95e7571e5f708012ac15
Pavel Elfimov <pelfimov@yandex.ru>
2021-07-08 14:30:17 +0700

    add simple component

---
+++ src/components/simple/why_we.tsx
@@ -0,0 +1,9 @@
+import {Component} from "react"
+import styles from "./simple.module.css"
+
+interface Props {}
+export class Simple extends Component<Props, {}> {
+  render() {
+    return <section className={styles.root} id="advantages"></section>
+  }
+}
