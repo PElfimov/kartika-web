406ea229f66cc1491c58ab9043f8fe1c7fca8cbb
Pavel Elfimov <pelfimov@yandex.ru>
2021-07-08 14:40:24 +0700

    add styles to price

--- src/components/price/price.tsx
+++ src/components/price/price.tsx
@@ -4,6 +4,25 @@ import styles from "./price.module.css"
 interface Props {}
 export class Price extends Component<Props, {}> {
   render() {
-    return <section className={styles.root}></section>
+    return (
+      <section className={styles.root}>
+        <div className={styles.inner}>
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              <p className={styles.name}>изготовление</p>
+              <p className={styles.param}>от 1 дня</p>
+            </li>
+            <li className={styles.item}>
+              <p className={styles.name}>цена за штуку</p>
+              <p className={styles.param}>от 3,95 рублей</p>
+            </li>
+            <li className={styles.item}>
+              <p className={styles.name}>изготовили более</p>
+              <p className={styles.param}>1 000 000 карт</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+    )
   }
 }
