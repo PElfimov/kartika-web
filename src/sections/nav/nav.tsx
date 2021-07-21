aadbbab9e5ab37345f1d405d452289538ca9f44a
Pavel Elfimov <pelfimov@yandex.ru>
2021-07-21 15:33:22 +0700

    add react to nav

--- src/sections/nav/nav.tsx
+++ src/sections/nav/nav.tsx
@@ -33,3 +33,13 @@ export class Nav extends Component<Props, {}> {
     )
   }
 }
+
+// jQuery(window).scroll(function() {
+//   var the_top = jQuery(document).scrollTop();
+//   if (the_top > 667) {
+//       jQuery('.main-navigation').addClass('main-navigation--fixed');
+//   }
+//   else {
+//       jQuery('.main-navigation').removeClass('main-navigation--fixed');
+//   }
+// });
