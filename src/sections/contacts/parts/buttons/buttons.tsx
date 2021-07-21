e65eee9ecfa505ea5dfd8819f292f1be53b53c1e
Pavel Elfimov <pelfimov@yandex.ru>
2021-07-21 14:17:55 +0700

    add styles to contact button

--- src/sections/contacts/parts/buttons/buttons.tsx
+++ src/sections/contacts/parts/buttons/buttons.tsx
@@ -1,33 +1,47 @@
 import {Component} from "react"
 import styles from "./buttons.module.css"
 
+type Item = {name: `vk` | `insta` | `ok` | `fb`; link: string; text: string}
+
+const item: Item[] = [
+  {
+    name: "vk",
+    link: "http://vk.com/t_kartika",
+    text: "Наша страничка В Контакте"
+  },
+  {
+    name: "insta",
+    link: "https://www.instagram.com/brn_kartika",
+    text: "Наша страничка Инстаграмм"
+  },
+  {
+    name: "ok",
+    link: "http://ok.ru/kartika",
+    text: "Наша страничка Онокласники"
+  },
+  {
+    name: "fb",
+    link: "https://www.facebook.com/kartika.barnaul",
+    text: "Наша страничка Фейсбук"
+  }
+]
+
 interface Props {}
 export class Buttons extends Component<Props, {}> {
   render() {
     return (
       <div className={styles.root}>
         <span className={styles.text}>Мы в социальных сетях</span>
-        <ul className="cocial-btn__list">
-          <li className="cocial-btn__item cocial-btn__item--vk">
-            <a href="http://vk.com/t_kartika" target="_blank">
-              <span className="cocial-btn__visually-hidden">Наша страничка В Контакте</span>
-            </a>
-          </li>
-          <li className="cocial-btn__item cocial-btn__item--insta">
-            <a href="https://www.instagram.com/brn_kartika" target="_blank">
-              <span className="cocial-btn__visually-hidden">Наша страничка Инстаграмм</span>
-            </a>
-          </li>
-          <li className="cocial-btn__item cocial-btn__item--ok">
-            <a href="http://ok.ru/kartika" target="_blank">
-              <span className="cocial-btn__visually-hidden">Наша страничка Онокласники</span>
-            </a>
-          </li>
-          <li className="cocial-btn__item cocial-btn__item--fb" cocial-btn__item--fb>
-            <a href="https://www.facebook.com/kartika.barnaul" target="_blank">
-              <span className="cocial-btn__visually-hidden">Наша страничка Фейсбук</span>
-            </a>
-          </li>
+        <ul className={styles.list}>
+          {item.map((item, index) => {
+            return (
+              <li key={`${index}-${item.name}`} className={`${styles.item} ${styles[item.name]}`}>
+                <a href={item.link} target="_blank">
+                  <span className={styles.hidden}>{item.text}</span>
+                </a>
+              </li>
+            )
+          })}
         </ul>
       </div>
     )
